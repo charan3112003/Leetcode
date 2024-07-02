c819c59e8428e293a5206d6fd2f37bdda6f9037a
charan3112003 <128051861+charan3112003@users.noreply.github.com>
2024-07-02 14:53:27 +0530

    Time: 2 ms (79.78%) | Memory: 46.6 MB (26.53%) - LeetSync

--- 1254-deepest-leaves-sum/deepest-leaves-sum.java
+++ 1254-deepest-leaves-sum/deepest-leaves-sum.java
@@ -18,12 +18,12 @@ class Solution {
     public int deepestLeavesSum(TreeNode root) {
         //int sum=0;
         int dep=maxdepth(root);
-        int ans=leavesum(dep,root,1);
+        int ans=leavesum(dep,root,0);
         return ans;
     }
     public static int maxdepth(TreeNode root){
         if(root==null){
-            return 0;
+            return -1;
         }
         return Math.max(maxdepth(root.left),maxdepth(root.right))+1;
     }
