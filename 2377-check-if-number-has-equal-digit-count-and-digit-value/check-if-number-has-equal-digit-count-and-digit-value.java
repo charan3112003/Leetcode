8f830c906d3b49776f43f17ad659b4dd260a91d6
charan3112003 <128051861+charan3112003@users.noreply.github.com>
2024-07-04 16:47:59 +0530

    Time: 2 ms (26.51%) | Memory: 42.3 MB (5.24%) - LeetSync

--- 2377-check-if-number-has-equal-digit-count-and-digit-value/check-if-number-has-equal-digit-count-and-digit-value.java
+++ 2377-check-if-number-has-equal-digit-count-and-digit-value/check-if-number-has-equal-digit-count-and-digit-value.java
@@ -1,23 +1,23 @@
 class Solution {
     public boolean digitCount(String num) {
         HashMap<Integer,Character> map=new HashMap<>();
-        char [] arr=new char[num.length()];
-        for(int i=0;i<num.length();i++){
-            arr[i]=num.charAt(i);
-        }
+        // char [] arr=new char[num.length()];
+        // for(int i=0;i<num.length();i++){
+        //     arr[i]=num.charAt(i);
+        // }
         for(int i=0;i<num.length();i++){
            // int ch=num.charAt(i);
             map.put(i,num.charAt(i));
         }
-        for(Map.Entry<Integer,Character> mp:map.entrySet()){
-            System.out.print(mp.getKey()+" "+mp.getValue()+" ");
-        }
-
+        // for(Map.Entry<Integer,Character> mp:map.entrySet()){
+        //     System.out.print(mp.getKey()+" "+mp.getValue()+" ");
+        // }
+        
         for(Integer j: map.keySet()){
             char a=map.get(j);
             int count=a-'0';
             int k=j;
-            for(int i=0;i<arr.length;i++){
+            for(int i=0;i<num.length();i++){
                 int o=num.charAt(i)-'0';
                 if(k==o){
                     count--;
