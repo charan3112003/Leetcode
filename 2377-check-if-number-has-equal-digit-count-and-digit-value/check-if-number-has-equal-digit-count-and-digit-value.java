a5b50b131a9d44f5b3f2599ec6d6e5a584b0e6bc
charan3112003 <128051861+charan3112003@users.noreply.github.com>
2024-07-04 16:34:41 +0530

    Time: 15 ms (5.87%) | Memory: 43 MB (5.24%) - LeetSync

---
+++ 2377-check-if-number-has-equal-digit-count-and-digit-value/check-if-number-has-equal-digit-count-and-digit-value.java
@@ -0,0 +1,37 @@
+class Solution {
+    public boolean digitCount(String num) {
+        HashMap<Integer,Character> map=new HashMap<>();
+        char [] arr=new char[num.length()];
+        for(int i=0;i<num.length();i++){
+            arr[i]=num.charAt(i);
+        }
+        for(int i=0;i<num.length();i++){
+           // int ch=num.charAt(i);
+            map.put(i,num.charAt(i));
+        }
+        for(Map.Entry<Integer,Character> mp:map.entrySet()){
+            System.out.print(mp.getKey()+" "+mp.getValue()+" ");
+        }
+
+        for(Integer j: map.keySet()){
+            char a=map.get(j);
+            int count=a-'0';
+            int k=j;
+            for(int i=0;i<arr.length;i++){
+                int o=num.charAt(i)-'0';
+                if(k==o){
+                    count--;
+                }   
+            }
+            if(count!=0){
+                return false;
+            }
+            
+        }
+
+        // for(Map.Entry<Integer,Character> mp:map.entrySet()){
+        //     System.out.print(mp.getKey()+" "+mp.getValue()+" ");
+        // }
+        return true;
+    }
+}
