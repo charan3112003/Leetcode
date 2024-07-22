3c1acf84c9f9fd2353f2c52ad21dfbc5cc625dc1
Charan M <charan3manoharan@gmail.com>
2024-07-22 23:33:39 +0530

    Time: 8 ms (75.41%) | Memory: 42.3 MB (81.18%) - LeetSync

---
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int characterReplacement(String s, int k) {
+        int [] occur=new int[26];
+        int l=0,r=0,ans=0;
+        int max=0;
+        for(r=0;r<s.length();r++){
+            max=Math.max(max,++occur[s.charAt(r)-'A']);
+            if(r-l+1-max>k){
+              
+                occur[s.charAt(l)-'A']--;
+                  l++;
+            }
+            ans=Math.max(ans,r-l+1);
+        }
+        return ans;
+    }
+}
